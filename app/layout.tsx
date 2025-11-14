71e42509788190f3a3281c1b9390720e5e89dd44
Rutuparn Kakade <134830170+rutuparnthestoic@users.noreply.github.com>
2025-11-14 13:40:12 +0530

    Add favicon link to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="shortcut icon" href="/public/logo-z.jpeg" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
